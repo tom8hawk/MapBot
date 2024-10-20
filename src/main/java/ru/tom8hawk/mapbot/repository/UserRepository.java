f4c72c9a3c54122b058facdd7c61b2372ce8c3c3
tom8hawk <tom8hawk@duck.com>
2024-10-21 00:02:38 +0300

    Минорное обновление

--- src/main/java/ru/tom8hawk/mapbot/repository/UserRepository.java
+++ src/main/java/ru/tom8hawk/mapbot/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package ru.tom8hawk.mapbot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.tom8hawk.mapbot.model.User;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByTelegramId(Long telegramId);
 }
